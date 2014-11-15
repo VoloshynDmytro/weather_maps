c15d7bcc9d5ddc32c31c501b26ac812ef251ce0e
VoloshynDmytro <voloshyn4@gmail.com>
2014-11-15 14:21:53 +0200

    Config rails environments for heroku

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module Weather_maps
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-     config.i18n.default_locale = :en
+    config.i18n.default_locale = :en
     root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
       config.sass.load_paths << bower_path
       config.assets.paths << bower_path
@@ -40,5 +40,17 @@ module Weather_maps
     config.assets.precompile << %r(bootstrap-sass-official/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff)$)
     # Minimum Sass number precision required by bootstrap-sass
     ::Sass::Script::Number.precision = [10, ::Sass::Script::Number.precision].max
+
+    # We don't want the default of everything that isn't js or css, because it pulls too many things in
+    config.assets.precompile.shift
+
+    # Explicitly register the extensions we are interested in compiling
+    config.assets.precompile.push(Proc.new do |path|
+      File.extname(path).in? [
+                                 '.html', '.erb', '.haml',                 # Templates
+                                 '.png',  '.gif', '.jpg', '.jpeg', '.svg', # Images
+                                 '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+                             ]
+    end)
   end
 end
